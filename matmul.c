82927a0d41fdc4aa6609efa7195817feee061fcb
vienvan <vienvonvan@gmail.com>
2018-05-22 13:10:34 -0700

    blocking is working, need to pass param via cli

--- matmul.c
+++ matmul.c
@@ -6,10 +6,16 @@
 
 #define SIZE 1024
 
+ #define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 __uint64_t A[SIZE][SIZE];
 __uint64_t B[SIZE][SIZE];
 __uint64_t C[SIZE][SIZE];
 __uint64_t D[SIZE][SIZE];
+__uint64_t E[SIZE][SIZE];
 __uint64_t tB[SIZE][SIZE];
 
 void transpose(__uint64_t B[][SIZE], __uint64_t tB[][SIZE])
@@ -83,8 +89,6 @@ void matmul(__uint64_t A[][SIZE], __uint64_t B[][SIZE])
 
 void transpose_matmul(__uint64_t A[][SIZE], __uint64_t tB[][SIZE])
 {
-	// printf("inside transpose_matmul, tB is: \n");
-	// print_matrix(tB);
 	int rowA, rowB, idx;
 	for (rowA = 0; rowA < SIZE; rowA++) {
 		for(rowB = 0; rowB < SIZE; rowB++ ){
@@ -95,8 +99,27 @@ void transpose_matmul(__uint64_t A[][SIZE], __uint64_t tB[][SIZE])
 	}
 }
 
-int main(int argc, char **argv)
+void blocking(__uint64_t A[][SIZE], __uint64_t tB[][SIZE], int block_size) 
 {
+	int i, k, j, jj, kk;
+	for (k = 0; k < SIZE; k += block_size)
+		for (j = 0; j < SIZE; j += block_size)
+			for (i = 0; i < SIZE; ++i)
+				for (jj = j; jj < min(j + block_size, SIZE); ++jj)
+					for (kk = k; kk < min(k + block_size, SIZE); ++kk)
+						E[i][jj] += A[i][kk] * tB[jj][kk];
+}
+int main(int argc, char *argv[])
+{
+	int block_size;
+	// printf("%d ", argc);
+	// printf("%s ", argv[0]); 
+	// printf("%s ", argv[1]);
+
+	if(argc == 2) {
+		printf("argv[1] %s", argv[1]);
+		block_size = (int) argv[1];
+	}
 	clock_t t;
 	clock_t t1;
 	double time_taken;
@@ -109,31 +132,35 @@ int main(int argc, char **argv)
 	// print_matrix(tB);
 	memset(C, 0, sizeof(__uint64_t) * SIZE * SIZE);
 	memset(D, 0, sizeof(__uint64_t) * SIZE * SIZE);
+	memset(E, 0, sizeof(__uint64_t) * SIZE * SIZE);
 	// printf("A: \n");
 	// print_matrix(A);
 	// printf("B: \n");
 	// print_matrix(B);
 	// printf("tB: \n");
 	// print_matrix(tB);
-	t = clock();
+	// t = clock();
 	// printf("A*B: \n");
-	matmul(A, B);
-	t = clock() - t;
-	time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
+	// matmul(A, B);
+	// t = clock() - t;
+	// time_taken = ((double)t)/CLOCKS_PER_SEC; // in seconds
 	// print_matrix(C);
-	printf("Matmul took %f seconds to execute \n", time_taken);
+	// printf("Matmul took %f seconds to execute \n", time_taken);
 	// printf("A*tB: \n");
 	// transpose_matmul(A, tB);
 	// print_matrix(D);
 
 	t1 = clock();
-	transpose_matmul(A, tB);
+	printf("%d \n", block_size);
+	blocking(A, tB, 64);
 	t1 = clock() - t1;
 	time_transposed_taken = ((double)t1)/CLOCKS_PER_SEC; // in seconds
 
-	verified = verify(C, D);
+	// blocking(A, tB);
 
-	printf("verified %d \n", verified);
+	// verified = verify(C, E);
 
-	printf("transposeMatmul took %f seconds to execute \n", time_transposed_taken);
+	// printf("verified %d \n", verified);
+
+	printf("blocking took %f seconds to execute \n", time_transposed_taken);
 }
